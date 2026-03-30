91d83750022876284705ac7be69bd33e4b083528
Alejandro Sánchez M. <alejandro70804@outlook.com>
2026-03-30 15:06:49 -0600

    Arrangement of exercise 1

--- Intermediate_Python/Exercises_Of_OOP/Exercise1.py
+++ Intermediate_Python/Exercises_Of_OOP/Exercise1.py
@@ -1,13 +1,13 @@
 class Circle:
-    radius = 12
+    def __init__(self, radius):
+        self.radius = radius
 
     def get_area(self):
         return 3.14 * (self.radius**2)
         
 
-area_1 = Circle()
+area_1 = Circle(12)
 print(area_1.get_area())
 
-area_2 = Circle()
-area_2.radius = 25
+area_2 = Circle(25)
 print(area_2.get_area())
